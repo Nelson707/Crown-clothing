5d94a8e9b0a50194f3ca48e3b8d9e13746cc840d
Nelson707 <nelsonnebo84@gmail.com>
2024-04-09 11:23:19 +0300

    warning changes

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
-import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
+import Button from '../button/button.component';
 
 import CartItem from '../cart-item/cart-item.component';
 
@@ -29,7 +29,7 @@ const CartDropdown = () => {
                 )
             }
             </CartItems>
-            <Button onClick={goToCheckoutHandler}>Go To Checkout</Button>
+            <Button onClick={goToCheckoutHandler}>Checkout</Button>
         </CartDropdownContainer>
     );
 };
